9c08d11607cd52912f8f92ba2e62cf61e3dad916
Jonathan Werner <jwerner@posteo.de>
2017-03-06 11:47:14 +0100

    add extract-text-webpack-plugin

--- lessModuleSVRLoader.js
+++ lessModuleSVRLoader.js
@@ -1,19 +1,25 @@
 const StringReplacePlugin = require('string-replace-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
 const createLessSVRLoader = require('./internal/createLessSVRLoader');
 
-module.exports = ({styles}) => (webpackConfig) => {
+module.exports = ({styles, production, cssFileName}) => (webpackConfig) => {
   const lessSVRLoader = createLessSVRLoader({styles});
   const cssModulesOptions = '&localIdentName=[path][name]__[local]--[hash:base64:5]&sourceMap=true&importLoaders=2';
+  const wrapExtractTextStyle = (loaders) => ExtractTextPlugin.extract('style-loader', loaders, {publicPath: ''});
 
   const loader = {
     test: /\.less/,
-    loader: `style-loader!css-loader?modules=true${cssModulesOptions}!postcss-loader!${lessSVRLoader}`,
+    loader: wrapExtractTextStyle(`css-loader?modules=true${cssModulesOptions}!postcss-loader!${lessSVRLoader}`),
   };
 
   webpackConfig.module.loaders.push(loader);
   webpackConfig.plugins.push(new StringReplacePlugin());
+  webpackConfig.plugins.push(new ExtractTextPlugin(
+    cssFileName || '[name]-[contenthash].css',
+    {allChunks: true, disable: !production} // eslint-disable-line comma-dangle
+  ));
   webpackConfig.postcss = () => {
     return [
       autoprefixer,
