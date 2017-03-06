172f8a210bd4dd77fe34476c51bd73821a28a200
Jonathan Werner <jwerner@posteo.de>
2017-03-06 11:04:39 +0100

    refactor to babel-preset-mesaic

--- babelLoader.js
+++ babelLoader.js
@@ -4,8 +4,7 @@ module.exports = () => (webpackConfig) => {
     exclude: /node_modules(?!\/react-atomic)/,
     loader: 'babel-loader',
     query: {
-      presets: ['es2015', 'react', 'stage-0'],
-      plugins: ['react-autoprefix', 'transform-runtime'],
+      presets: ['mesaic'],
     },
   });
 };
