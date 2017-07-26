76c13e21511e2f4d6a87c2c42f53fbabe792c748
Niko Uphoff <nikuph@gmail.com>
2017-07-26 17:09:49 +0200

    feat: remove empty string from extensions array

--- extensions.js
+++ extensions.js
@@ -1,3 +1,3 @@
 module.exports = (additionalExtensions) => (webpackConfig) => {
-  webpackConfig.resolve.extensions = ['', '.js', '.svg', '.css', '.less', '.less.module', '.json'].concat(additionalExtensions || []);
+  webpackConfig.resolve.extensions = ['.js', '.svg', '.css', '.less', '.less.vars', '.json'].concat(additionalExtensions || []);
 };
