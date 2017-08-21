4bcaea105898335ec003a7c6d1862562cc6e1261
Niko Uphoff <nikuph@gmail.com>
2017-08-21 19:03:41 +0200

    fix: update module name also for loader resolving

--- resolveLoader.js
+++ resolveLoader.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = () => (webpackConfig) => {
   webpackConfig.resolveLoader = {
     modules: [
-      path.join(process.cwd(), 'node_modules/mesaic-webpack/node_modules'),
-      path.join(process.cwd(), 'node_modules/mesaic-webpack'),
+      path.join(process.cwd(), 'node_modules/@mesaic/mesaic-webpack/node_modules'),
+      path.join(process.cwd(), 'node_modules/@mesaic/mesaic-webpack'),
       path.join(process.cwd(), 'node_modules'),
     ],
   };
